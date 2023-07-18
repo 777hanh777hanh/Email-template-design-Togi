a4a61a372df099bfada0f2ed68306972e4e51fde
777hanh777hanh <thanhthientran151100@gmail.com>
2023-07-18 17:51:37 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import thumbnail from './assets/images/thumb.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="heading">Project 11:</h1>
+            <h3 className="desc">Email template design Togi</h3>
+            <img
+                src={thumbnail}
+                alt=""
+                className="thumbnail"
+                style={{ display: 'block', margin: '0 auto', width: '70%' }}
+            />
+        </div>
+    );
 }
 
 export default App;
